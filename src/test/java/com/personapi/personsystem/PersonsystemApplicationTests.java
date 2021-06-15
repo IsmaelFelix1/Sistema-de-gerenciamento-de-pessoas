faad7b53e8aed1578d3644ca84724754033e2b7d
Ismael Felix <ismael.duarte017@gmail.com>
2021-06-15 12:49:08 -0300

    Project created

---
+++ src/test/java/com/personapi/personsystem/PersonsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personapi.personsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
